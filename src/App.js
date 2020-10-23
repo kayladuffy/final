ce87d07e38c35f28d86c5f5b07c70997fd5f1f8c
kayladuffy <kaylamorganduffy@gmail.com>
2020-10-22 20:28:05 -0500

    Added sidebar navigation; intended for catergories

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import { NavigationBar } from './components/NavigationBar';
 import { Home } from './Home';
 import { About } from './About';
 import { NoMatch } from './NoMatch';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <React.Fragment>
       <Router>
         <NavigationBar />
+        <Sidebar />
 
         <Switch>
           <Route exact path="/" component={Home} />
